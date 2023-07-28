f85bd815a6a999f386e176875b9f71717ab8a475
kko3ch <tomko3ch@gmail.com>
2023-07-28 12:28:11 +0300

    added API

--- my_app/urls.py
+++ my_app/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="home"),
+  path('', views.index, name="index"),
 ]
+
+urlpatterns += static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
--- my_app/views.py
+++ my_app/views.py
@@ -1,4 +1,29 @@
 from django.shortcuts import render
+import requests
+import datetime
+from decouple import config
+import urllib.request
+import json
 
-def home(request):
-    render
+
+def index(request):
+    if request.method == 'POST':
+        city = request.POST['city']
+        source = urllib.request.urlopen('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=metric&appid=b00877cb0084bcabf270ee6fa62a61a5').read()
+        list_of_data = json.loads(source)
+
+        data  = {
+            "country_code": str(list_of_data['sys']['country']),
+            "coordinate": str(list_of_data['coord']['lon']) + ',' + str(list_of_data['coord']['lat']),
+            'temp': str(list_of_data['main']['temp']) + '  °C',
+            'pressure': str(list_of_data['main']['pressure']),
+            'humidity': str(list_of_data['main']['humidity']),
+            'main': str(list_of_data['weather'][0]['main']),
+            'description': str(list_of_data['weather'][0]['description']),
+            'icon': list_of_data['weather'][0]['icon']
+        }
+        # print(data)
+    else: 
+        data = {}
+
+    return render(request, "my_app/index.html", data)
